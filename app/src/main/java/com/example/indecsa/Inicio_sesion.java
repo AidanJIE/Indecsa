6fb166b130bc0435c200c47e58e1e2ea58590b58
Nina256-i <ninaislas25@gmail.com>
2025-11-27 23:53:34 -0600

    version 0.2.5.1

--- app/src/main/java/com/example/indecsa/Inicio_sesion.java
+++ app/src/main/java/com/example/indecsa/Inicio_sesion.java
@@ -7,9 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.fragment.app.Fragment;
-
 import com.example.indecsa.models.LoginRequestAdmin;
 import com.example.indecsa.models.LoginRequestCapHum;
 import com.example.indecsa.models.LoginResponse;
